ad354ffddf2e9bf44c2e89e60c53c1e67c7d58e7
Jesus varela <jesusvarelaredo@gmail.com>
2021-08-30 14:47:49 -0500

    actualizacion del proyeto

---
+++ flutter/README.md
@@ -0,0 +1,2 @@
+# proyecto-formativo-2021-woodfed
+proyecto-formativo-2021-woodfed created by GitHub Classroom
---
+++ flutter/android/app/src/main/kotlin/com/example/wood_fed/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.wood_fed
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
